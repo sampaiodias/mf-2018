d01a7cc4f4c0182bd4f34c15d67c0b32a3c09ce7
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-09-17 19:07:22 -0300

    Aula 3: Aplicacao.java corrigido

--- aula-3/semana/Aplicacao.java
+++ aula-3/semana/Aplicacao.java
@@ -1,7 +1,9 @@
 import java.time.*;
+import java.util.Locale;
+import java.time.format.TextStyle;
 
 public class Aplicacao {
 	public static void main (String[] args) {
-		System.out.println(LocalDate.now().getDayOfWeek());
+		System.out.println(LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault()));
 	}
 }
