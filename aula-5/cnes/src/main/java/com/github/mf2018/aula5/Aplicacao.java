0d64b56511f4398af38e77a36eb1f5d9a8c16307
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-10-18 09:51:32 -0300

    Aula 5: projeto cnes

---
+++ aula-5/cnes/src/main/java/com/github/mf2018/aula5/Aplicacao.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018.
+ * Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+package com.github.mf2018.aula5;
+
+/**
+ * Mais detalhes no tópico "Aula 5" do documento disponível em:
+ * https://docs.google.com/document/d/1TTFjo0qcjmuaYkE1okWAiV_qqoyYs9r4FFO3dp7nikI/edit#
+ * @author Lucas Sampaio Dias
+ */
+public class Aplicacao {
+    public static void main(String[] args) {
+        
+    }
+}
