6c886e52fb770ee33644975986666b11683f3cff
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-11-05 20:06:41 -0200

    Aula 7: Diretório DTO

---
+++ aula-7/dto/src/main/java/com/github/mf2018/aula7/Aplicacao.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018.
+ * Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+package com.github.mf2018.aula7;
+
+/**
+ * 
+ * @author Lucas
+ */
+public class Aplicacao {
+    
+}
