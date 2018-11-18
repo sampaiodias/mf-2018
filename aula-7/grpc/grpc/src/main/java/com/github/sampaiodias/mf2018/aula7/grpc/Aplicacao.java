a2ffbf238f6e883ba04eaba9147a7ccba4fb0d37
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-11-18 15:38:09 -0200

    Aula 7: Identificador construído com Protobuf

--- aula-7/grpc/grpc/src/main/java/com/github/sampaiodias/mf2018/aula7/grpc/Aplicacao.java
+++ aula-7/grpc/grpc/src/main/java/com/github/sampaiodias/mf2018/aula7/grpc/Aplicacao.java
@@ -11,6 +11,36 @@ package com.github.sampaiodias.mf2018.aula7.grpc;
  */
 public class Aplicacao {
     public static void main(String[] args) {
+        IdentificadorOuterClass.Identificador id = buildIdentificador();
+        System.out.println(id.getAllFields());
+    }
+    
+    private static IdentificadorOuterClass.Identificador buildIdentificador() {
+        IdentificadorOuterClass.Identificador.Certidao c = 
+                IdentificadorOuterClass.Identificador.Certidao.newBuilder()
+                    .setCartorio("A")
+                    .build();
         
+        IdentificadorOuterClass.Identificador.TituloEleitoral t = 
+                IdentificadorOuterClass.Identificador.TituloEleitoral.
+                        newBuilder()
+                            .setSessao(1)
+                            .setZona(2)
+                            .build();
+        
+        IdentificadorOuterClass.Identificador id = IdentificadorOuterClass.
+                Identificador.newBuilder()
+                .setCodigoArea(12345)
+                .setCodigoTipo("TIPO")
+                .setData("20180101")
+                .setDesignacao("DESIGNACAO")
+                .setEmissor("EMISSOR")
+                .setId(1234)
+                .setIdIndividuo(123)
+                .setCertidao(c)
+                .setTituloEleitoral(t)
+                .build();
+        
+        return id;
     }
 }
