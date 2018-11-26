545033b896be28eb6a6ee0172260e3674b9b5b39
Lucas Sampaio Dias <lucassampaiodias@gmail.com>
2018-11-26 09:22:47 -0200

    Aula 8: Projeto Base

---
+++ aula-8/loinc-java/src/main/java/com/github/sampaiodias/mf2018/aula8/Loinc.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018.
+ * Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+package com.github.sampaiodias.mf2018.aula8;
+
+/**
+ *
+ * @author Lucas
+ */
+public class Loinc {
+    
+}
